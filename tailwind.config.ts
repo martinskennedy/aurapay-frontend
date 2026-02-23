e19b50eaad8a2373d55f6299339cd7bf4575a486
Kennedy Martins <kemartins@gmail.com>
2026-02-23 17:27:05 -0300

    chore: initial setup with nextjs, tailwind and design system

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#6366f1",
+          foreground: "#ffffff",
+        },
+        secondary: {
+          DEFAULT: "#a855f7",
+          foreground: "#ffffff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
